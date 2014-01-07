8afabea203925f13dcbc275146cd84477a10c03d
Nick Macioce <nmacioce@vt.edu>
2014-01-06 21:48:06 -0500

    Added assets (images for UI), configured user interface, and added 2 classes for Flickr API use.
    - NM on 1/6/2014

--- nrve For iPad/nrve For iPad/ViewController.m
+++ nrve For iPad/nrve For iPad/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+    @property(nonatomic, weak) IBOutlet UIToolbar *toolbar;
+    @property(nonatomic, weak) IBOutlet UIBarButtonItem *shareButton;
+    @property(nonatomic, weak) IBOutlet UITextField *textField;
+    - (IBAction)shareButtonTapped:(id)sender;
 @end
 
 @implementation ViewController
@@ -17,13 +20,28 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_cork.png"]];
+    
+    UIImage *navBarImage = [[UIImage imageNamed:@"navbar.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(27, 27, 27, 27)];
+    [self.toolbar setBackgroundImage:navBarImage forToolbarPosition:UIToolbarPositionAny
+                          barMetrics:UIBarMetricsDefault];
+    
+    UIImage *shareButtonImage = [[UIImage imageNamed:@"button.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(8, 8, 8, 8)];
+    [self.shareButton setBackgroundImage:shareButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    
+    UIImage *textFieldImage = [[UIImage imageNamed:@"search_field.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
+    [self.textField setBackground:textFieldImage];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)shareButtonTapped:(id)sender {
+    // TODO
 }
 
 @end
